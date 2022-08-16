7f5abf4aa89baf11b2ea4d85371f553a693f4568
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2022-08-16 10:07:45 -0400

    table structures

--- js/database.js
+++ js/database.js
@@ -5,7 +5,7 @@ const Database = require('better-sqlite3');
 
 // Connect to a database or create one if it doesn't exist yet.
 const db = new Database('./db/user.db');
-
+db.exec('PRAGMA foreign_keys = ON')
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
 let row = stmt.get();
@@ -13,14 +13,41 @@ if (row === undefined) {
 // Echo information about what you are doing to the console.
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
+
+/* 
+
+userinfo
+_____________________________________________________________________________________________
+| id                    | user          | pass  | email         | date_created  | public    |
+| INTEGER PRIMARY KEY   | TEXT UNIQUE   | TEXT  | TEXT UNIQUE   | INTEGER       | INTEGER   |
+_____________________________________________________________________________________________
+
+
+ weight
+_________________________________________________________________________________
+| id                            | userid        | weight        | date_entered  |
+| INTEGER PRIMARY KEY NOT NULL  | FOREIGN KEY   | REAL NOT NULL | INTEGER       |
+_________________________________________________________________________________
+
+*/
     const sqlInit = `
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, user TEXT UNIQUE, pass TEXT, cookies INT, clickers INT);
-		INSERT INTO userinfo (user, pass) VALUES ('admin','bdc87b9c894da5168059e00ebffb9077'), ('test','9241818c20435c6672dac2c4b6e6c071')
+        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                user TEXT UNIQUE, 
+                                pass TEXT, 
+                                email TEXT UNIQUE,
+                                date_created INTEGER
+                                public INTEGER NOT NULL DEFAULT 0,
+                                CHECK (public IN (0,1)));
+        CREATE TABLE weight (   weight_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                user_id INTEGER NOT NULL, 
+                                weight REAL NOT NULL, 
+                                date_entered INTEGER,
+                                FOREIGN KEY (user_id) REFERENCES userinfo (id) ON DELETE CASCADE ON UPDATE CASCADE);
     `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
 // Echo information about what we just did to the console.
-    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+    console.log('Your database has been initialized with a new table and one entry containing a username and password.');
 } else {
 // Since the database already exists, echo that to the console.
     console.log('Database exists.')
