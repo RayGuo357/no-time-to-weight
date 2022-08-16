c76886ef06ac5eb7baddf8ae00227cfc751c428c
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2022-08-16 10:04:41 -0400

    time and date function

--- js/helper.js
+++ js/helper.js
@@ -48,4 +48,17 @@ const randomQuotes = (x) => {
     return result;
 }
 
-module.exports = { randomQuotes }
+
+const getTodaysDate = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    var time = String(today.getHours()).padStart(2, '0') + ":" 
+                + String(today.getMinutes()).padStart(2, '0') + ":" 
+                + String(today.getSeconds()).padStart(2, '0');
+
+    return `${yyyy}-${mm}-${dd} ${time}`
+}
+
+module.exports = { randomQuotes, getTodaysDate }
