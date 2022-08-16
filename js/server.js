bb52dfcb50e2356d943180b08ce4408aaa86bc08
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2022-08-16 10:08:07 -0400

    add endpoints

--- js/server.js
+++ js/server.js
@@ -11,16 +11,38 @@ app.set('view engine', 'ejs')
 
 const helper = require('./helper.js')
 
+// Start page
 app.get('/', (req, res) => {
     res.render('index.ejs', {quotes: helper.randomQuotes(3)})
 })
 
+// Login page
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/pages/login.html')
+    res.sendFile('login.ejs')
 })
 
+// Sign up page
 app.get('/signup', (req, res) => {
-    res.sendFile(__dirname + '/pages/signup.html')
+    res.render('signup.ejs')
+})
+
+// Endpoint for account creation
+app.post('/signup/submit', (req, res) => {
+    const stmt = db.prepare('INSERT INTO userinfo (user, pass, email, date_created) VALUES (?, ?, ?, ?)');
+    try {
+        const info = stmt.run(req.body.user, req.body.pass, req.body.email, helper.getTodaysDate());
+        if(info.changes === 1) {
+            res.status(201).json({message:"1 record created: ID " + info.lastInsertRowid + " (201)"})
+        } else {
+            res.status(400).json({message:"Bad Request (400)"})
+        }
+    } catch (error) {
+        res.status(400).json({message:"Bad Request (400)", "error": error}) // remove error
+    }
+})
+
+app.get('/home', (req, res) => {
+    res.render('home.ejs')
 })
 
 app.listen(PORT, () => {
